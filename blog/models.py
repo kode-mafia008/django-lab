161795022334add71ad703207556475b6755e173
menuka <cmenuka60@gmail.com>
2026-08-26 13:36:53 +0545

    Workshop day2

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models 
+
+class Author(models.Model):
+    name = models.CharField(max_length = 100)
+    bio = models.TextField(blank = True)
+
+    class Meta:
+        db_table = 'authors'      
+
+
--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #userdefined and third party apps
     'catalog',
+    'blog',
 ]
 
 MIDDLEWARE = [
