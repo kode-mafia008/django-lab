5877babc5f978c610dc5d6de89d4c9604c65ebe1
Sipendra Chaudhary <your-github-email@example.comsipendrachaudhary1@gmail.com>
2026-08-26 13:37:06 +0545

    day 2

---
+++ blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Author(models.Model):
+        name =models.CharField(max_length = 100)
+        bio =models.TextField(blank =True)
+
+        class Meta:    
+            db_table = 'authors'
