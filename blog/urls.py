48790751986908cca31bd61be5803b057cc49112
himanshu <himanshughosh600@gmail.com>
2026-08-27 13:33:34 +0545

    day3

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-urlpatterns = [
+from . import views
 
-    
+urlpatterns = [
+    path("list/", views.author_list, name="author_list")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Author
 
-# Create your views here.
+def author_list(request):
+    authors = Author.objects.all() 
+    print(authors.query)
+    return render(request, 'author-list.html', {'authors': authors})
