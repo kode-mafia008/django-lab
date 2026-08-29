a65e3d9ce39236d6a50f20c2df2d09fc960bf4e6
Your Name <you@example.com>
2026-08-29 11:43:54 +0545

    hello

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,9 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         db_table = "authors"
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Author
+
+
+def author_list(request):
+    authors= Author.objects.all()
+    return render(request,"author_list.html",{"authors":authors})
+
 
 # Create your views here.
