cc53c908e8ecd27a699f6774cc829290019734c3
Your Name <you@example.com>
2026-08-29 14:04:02 +0545

    workshopday4

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
 
     # user-defined apps
     'blog',
+    'drf_spectacular',
+    'accounts',
+     "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,23 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
+
