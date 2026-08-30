22e4c3abdb70c9c993770c8fe1cd5e4223c87591
Your Name <you@example.com>
2026-08-30 11:34:57 +0545

    nothing

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.password_validation import  validate_password
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
@@ -7,29 +7,35 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["id","username","email"]
+        Fields = ["id" , "username" , "email" ]
 
+class RegisterSerialuzer(serializers.ModelSerializer):
 
-class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
-    write_only = True,
-    validators=[validate_password]
-)
-class Meta:
-    model = User
-    fields = ["id","username","email","password"]
+        write_only = True,
+        validators = [validate_password]
+    )
+
+    class Meta:
+        model = User
+        fiels = ["id" , "username" , "email" , "password"]
 
 class LoginSerializer(TokenObtainPairSerializer):
     @classmethod
-    def get_token(cls, user):
-        return super().get_token(user)
-    token['username'] = user.username
-    return token    
+    def get_token(cls , user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
 
-    def validate(self,attrs):
+    def validate(self , attrs):
         data = super().validate(attrs)
-        data['user']= UserSerializer(self.user).data
+        data['user'] = UserSerializer(self.user).data
         return data
+
+class LoginResponseSerializer(serializers.Serializer):
+    access = serializers.CharField()
+    refresh = serializers.CharField()
+    user = UserSerializer()
 
     
 
