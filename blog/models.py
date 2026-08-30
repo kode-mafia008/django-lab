052832c55cd111040f0ad8d970c38705c546b642
ranjan <ranjanyadav73548@gmail.com>
2026-08-30 11:41:29 +0545

    Save day4 work

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,5 @@ class Author(models.Model):
     class Meta:
         db_table = "authors"
 
+    def __str__(self):
+        return self.name
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Author
+
 
 # Create your views here.
+def author_list(request):
+    authors = Author.objects.all() #ORM(OBJECT OPERATIONAL MAPPING)
+    return render(request, "author_list.html", {"authors": authors})
