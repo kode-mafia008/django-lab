c99b99f4e7a7098bdcb2ac8eab58cda03ce8e054
himanshu <himanshughosh600@gmail.com>
2026-08-31 14:02:57 +0545

    day 06

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,7 +43,7 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'accounts',
     'rest_framework',
-    
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 
