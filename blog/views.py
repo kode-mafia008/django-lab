7775df89e6ab479653feb57f1b563427d12047c1
kaushal-karna <kaushalkarnakayansh@gmail.com>
2026-09-01 12:11:09 +0545

    Updated in day6

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Author
+from .serializers import AuthorSerializer
 
 
 def author_list(request):
@@ -11,3 +13,12 @@ def author_list(request):
 def author_detail(request, pk):
     author = get_object_or_404(Author, pk=pk)
     return render(request, "blog/author_detail.html", {"author": author})
+
+
+class AuthorListView(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [IsAuthenticated]
+    
+    
+    
