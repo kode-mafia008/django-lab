e1315f4735215bc806d45536e035f95234fcf375
kaushal-karna <kaushalkarnakayansh@gmail.com>
2026-09-02 12:59:38 +0545

    Updated in day8

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from . import views
 
 
 app_name = "blog"
 
+router = DefaultRouter()
+router.register(r'blogs', views.BlogViewSet, basename='blog')
 
 urlpatterns = [
     path("", views.author_list, name="author-list"),
     path("authors/<int:pk>/", views.author_detail, name="author-detail"),
 
     # API endpoints(Class-based views)
-    path('api/authors',views.AuthorListView.as_view()),
+    # path('api/authors',views.AuthorListView.as_view()),
+    
+    path("api/", include(router.urls)),
 ]
