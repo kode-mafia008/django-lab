1a05438ad5994f9ebb2116d13233eb9b0e0ee1dc
menuka <cmenuka60@gmail.com>
2026-09-02 14:03:12 +0545

    Workshop Day 8

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import get_object_or_404, render
-from rest_framework import generics
+
 from rest_framework import viewsets
 from .serializers import AuthorSerializer, BlogSerializer
 from .models import Author, blog
 from rest_framework.permissions import IsAuthenticated,AllowAny 
+from drf_spectacular.utils import extend_schema
 
 def author_list(request):
     authors = Author.objects.order_by("name")
@@ -14,12 +15,15 @@ def author_detail(request, pk):
     author = get_object_or_404(Author, pk=pk)
     return render(request, "blog/author_detail.html", {"author": author})
 
+@extend_schema(tags=["Authors"])
+
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
     permission_classes = [IsAuthenticated]
 
+@extend_schema(tags=["Blogs"])
 class BlogViewSet(viewsets.ModelViewSet):
     queryset = blog.objects.all()
     serializer_class = BlogSerializer
